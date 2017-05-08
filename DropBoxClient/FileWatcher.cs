fa39008a65218b151e6e352cde7c5597cde1c3a8
sewemark <sewerynjakub.markowicz@gmail.com>
2017-05-08 19:37:08 +0200

    .NET rx installed , FileWatcher class added

---
+++ DropBoxClient/FileWatcher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DropBoxClient
+{
+    public class FileWatcher : IDisposable
+    {
+        private FileSystemWatcher fileSystemWatcher;
+        public FileWatcher(FileSystemWatcher _fileSystemWatcher)
+        {
+            fileSystemWatcher = _fileSystemWatcher;
+        }
+        public void Dispose()
+        {
+            fileSystemWatcher.Dispose();
+        }
+    }
+}
