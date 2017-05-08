64ef01efb962548ddd1806a297f36707547fe55f
sewemark <sewerynjakub.markowicz@gmail.com>
2017-05-08 21:06:32 +0200

    SubjectInfo class addded

--- DropBoxClient/DroppedFileWatcher.cs
+++ DropBoxClient/DroppedFileWatcher.cs
@@ -1,10 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Reactive.Subjects;
 
 namespace DropBoxClient
 {
     public class DroppedFileWatcher
     {
+        private string path;
+
+        private FileWatcher fileWatcher;
+        private readonly Subject<SubjectInfo> subject = new Subject<SubjectInfo>();
+        public DroppedFileWatcher(FileWatcher _fileWatcher, string _path)
+        {
+            path = _path;
+            fileWatcher = _fileWatcher;
+        }
+
+        
+
     }
 }
