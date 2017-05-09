98cb6fbcb29dd286bea9a28b3707c4b7eb7444e2
sewemark <sewerynjakub.markowicz@gmail.com>
2017-05-09 21:20:10 +0200

    System.Reactive installed

--- DropBoxClient/FileWatcher.cs
+++ DropBoxClient/FileWatcher.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
-
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
 namespace DropBoxClient
 {
     public class FileWatcher : IDisposable
     {
         private FileSystemWatcher fileSystemWatcher;
+        public IObservable<FileSystemEventArgs> Created { get; set; }
         public FileWatcher(FileSystemWatcher _fileSystemWatcher)
         {
+            
             fileSystemWatcher = _fileSystemWatcher;
+            //Created = Obser
         }
         public void Dispose()
         {
